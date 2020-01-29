a546b6cc1804f912ce3a9bbc41c233f81df9be6c
Miguel <mikelo_191@hotmail.com>
2020-01-29 15:07:57 +0100

    Primer Commit

---
+++ src/main/java/com/restFullWebService/example/ResTfullWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.restFullWebService.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfullWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfullWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restFullWebService/example/ResTfullWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restFullWebService.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfullWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
